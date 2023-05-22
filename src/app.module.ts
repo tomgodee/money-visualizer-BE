7d8ceaf7fdc6238eb397f0a0019a665fbf265a1e
tomgodee <trungvu8295@gmail.com>
2023-05-22 16:22:37 +0700

    Run migration on deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dataSourceConfig } from './datasource';
 import { EntriesModule } from './entries/entries.module';
+
+import { DataSourceOptions } from 'typeorm';
+import { Entry } from './entries/entry.entity';
+import 'dotenv/config'; // this would make .env file works
+
+// Redefine dataSourceConfig since for some reason deployment doesnt recognize the datasource when reading this file
+export const dataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Entry],
+  // entities: ['../**/*.entity.{ts,js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  migrationsRun: true,
+};
 
 @Module({
   imports: [
